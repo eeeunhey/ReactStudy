1a2d80251cde37b5fb525910707a9ece98b5bc37
eunhye <dmsgp2627@naver.com>
2025-10-21 20:17:42 +0900

    config 설정

--- 251020/vite.config.js
+++ 251020/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+const base = process.env.BASE_PATH || '/';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/251020/' 
+  base: '/ReactStudy/' 
 })
